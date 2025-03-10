cbf7f3aebfbabfa7cab4516c0965d89f5c2aaf53
Teenu2105 <priyankateenu2105@gmail.com>
2025-03-10 14:50:51 +0530

    updated

--- database/Subject table queries.sql
+++ database/Subject table queries.sql
@@ -4,193 +4,197 @@ In subject table
 
 2nd Sem(SEP)
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(100,'Data Structure',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(100,'Data Structure',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(101,'Object-Oriented Programming Using JAVA',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(101,'Object-Oriented Programming Using JAVA',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(102,'Operating system',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(102,'Operating system',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(103,'Data Structure Lab',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(103,'Data Structure Lab',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(104,'Object-Oriented Programming Lab',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(104,'Object-Oriented Programming Lab',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(105,'LINUX and Shell Programming Lab',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(105,'LINUX and Shell Programming Lab',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(106,'Language L1',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(106,'Language L1',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(107,'Language L2',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(107,'Language L2',2,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(108,'The Constitution of India ',2,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(108,'The Constitution of India ',2,1);
 
 
 
 4nd Sem(SEP)
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(109,'Computer Networks',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(109,'Computer Networks',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(110,'Design and Analysis of Algorithms',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(110,'Design and Analysis of Algorithms',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(111,'Software Engineering',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(111,'Software Engineering',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(112,'Computer Networks Lab',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(112,'Computer Networks Lab',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(113,' Design and Analysis of Algorithms Lab',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(113,' Design and Analysis of Algorithms Lab',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(114,'Elective:II Data Visualization',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(114,'Elective:II Data Visualization',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(115,'Language L1',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(115,'Language L1',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(116,'Language L2',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(116,'Language L2',4,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(117,'Office Management Tools',4,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(117,'Office Management Tools',4,1);
 
 6nd Sem(SEP)
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(118,'Big Data Analysis',6,'SEP');
+
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(118,'Big Data Analysis',6,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(119,'Exploratory Data Analysis ',6,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(119,'Exploratory Data Analysis ',6,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(120,'Project Work',6,'SEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(120,'Project Work',6,1);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(121,'Soft Skills',6,'SEP');
+INSERT INTO
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(121,'Soft Skills',6,1);
 
 2nd Sem(NEP)
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(122,'Computer Architecture',2,'NEP');
+INSERT INTO
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(122,'Computer Architecture',2,0);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(123,'Object Oriented Programming Using Java',2,'NEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(123,'Object Oriented Programming Using Java',2,0);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(124,'Database Management System',2,'NEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(124,'Database Management System',2,0);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(125,'Java Lab',2,'NEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(125,'Java Lab',2,0);
 
 
-INSERT INTO Subject(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES(126,'Database Management system Lab',2,'NEP');
+INSERT INTO Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES(126,'Database Management system Lab',2,0);
 
 4th Sem (NEP)
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (127,’Software Engineering’,4,’NEP’);
+INSERT INTO
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (127,’Software Engineering’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (128,’Design and Analysis of Algorithm ’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (128,’Design and Analysis of Algorithm ’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (129,’Internet Technologies’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (129,’Internet Technologies’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (130,’ Design and Analysis of Algorithm Lab’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (130,’ Design and Analysis of Algorithm Lab’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (131,’ Internet Technologies Lab’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (131,’ Internet Technologies Lab’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (132,’Open Elective’,4,’NEP’);
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (133,’Language L1’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (132,’Open Elective’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (134,’Language L2’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (133,’Language L1’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (136,’The Constitution of India’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (134,’Language L2’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (137,’Physical Education’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (136,’The Constitution of India’,4,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (138,’NSS/NCC/CL/R&R’,4,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (137,’Physical Education’,4,0);
+
+INSERT INTO
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (138,’NSS/NCC/CL/R&R’,4,0);
 
 6th Semester (NEP)
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (139,’Project Work’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (139,’Project Work’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (140,’Machine Learning’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (140,’Machine Learning’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (141,’Mobile Application Development’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (141,’Mobile Application Development’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (142,’ Machine Learning Lab’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (142,’ Machine Learning Lab’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (143,’Mobile Application Lab’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (143,’Mobile Application Lab’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (144,’CA-V2 Vocation course II : Electronic Content Design’,6,’NEP’);
+INSERT INTO
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (144,’CA-V2 Vocation course II : Electronic Content Design’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
 VALUES (145,’ CA-V2 Vocation course II :a. Operation Research b.Software 
-Testing’,6,’NEP’);
+Testing’,6,0);
 
-INSERT INTO Subject
-(subject_id,subject_name,subject_semester,nep_sep_syllabus)
-VALUES (146,’Internship’,6,’NEP’);
+INSERT INTO 
+Subject(subject_id,subject_name,subject_semester,is_sep)
+VALUES (146,’Internship’,6,0);
